6e72de211d8009da01ea0d7c228f2acc6d1325e3
matt knutson and tim durward <student@epicodus.com>
2016-03-14 15:03:58 -0700

    redo simon function to join array

--- js/simon.js
+++ js/simon.js
@@ -2,35 +2,37 @@
 //To Export Constructor use module.exports = ConstructorName (ref. line 23);
 
 function randomColor() {
-  var colors = ['blue', 'green', 'red', 'yellow'];
+  var colors = ['blue', 'green'];
   var rColor = Math.floor(Math.random() * (colors.length))
   return colors[rColor];
 }
 
 function simon(answer) {
   var counter = 3;
-  var answer = [];
-  var simonColors = [randomColor()];
-  if (counter > 0){
-    simonColors.push(randomColor());
+  var colorArray = [randomColor()];
 
-    if (answer == simonColors) {
+  if (counter > 0){
+    colorArray.push(randomColor());
+    var colors = colorArray.join(" ");
+
+      if (answer == colors) {
+        console.log(true);
+      }
+      else {
+        console.log(false);
+      }
+      return colorArray;
+  }
+
+  if (counter == 0) {
+    var colors = colorArray.join(" ");
+
+    if (answer == colors) {
       console.log(true);
     }
     else {
       console.log(false);
     }
-      return simonColors;
-  }
-
-  if (counter == 0) {
-
-    if (answer == simonColors) {
-      console.log(true);
-    }
-    else {
-      return false;
-    }
-      return simonColors;
+      return colorArray;
   }
 }
